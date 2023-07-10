3435788d47c0f579c5ede6337002cc1a2eba6a6b
AureliusIvan <aurelius.ivan@student.umn.ac.id>
2023-07-10 07:59:25 +0700

    added Icon

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,7 +4,7 @@ const props = defineProps({
         type: String,
         required: true,
     },
-    src: {
+    icon: {
         type: String,
         // required: true,
     },
@@ -13,12 +13,14 @@ const props = defineProps({
         // required: true,
     }
 });
+
+
 </script>
 
 <template>
     <a class="container" :href=href>
         <div>
-            <img :src="src">
+            <img :src="icon" alt="icon" />
         </div>
         <div>
             <h1>{{ name }}</h1>
@@ -39,13 +41,23 @@ const props = defineProps({
     margin: 10px;
     border-radius: 10px;
     padding: 10px;
-    width: 80px;
-    height: 80px;
+    width: 100%;
+    aspect-ratio: 1/1;
+    height: 100%;
+    transition: 0.3s;
+
+    @media screen and (max-width: 1000px) {
+        width: calc(40% - 20px);
+    }
+}
+
+.container:hover {
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.5);
 }
 
 
 h1 {
-    font-size: 10px;
+    font-size: 70%;
     font-weight: 700;
     text-align: center;
     margin: 0;
@@ -54,7 +66,12 @@ h1 {
 }
 
 img {
-    width: 20px;
-    height: 20px;
+    width: 100%;
+    aspect-ratio: 1/1;
+}
+
+i {
+    font-size: 40px;
+    color: rgb(79, 79, 79);
 }
 </style>
--- pages/kontak.vue
+++ pages/kontak.vue
@@ -1,17 +1,48 @@
 <script setup lang="ts">
 import json from '@/content.json';
-const socialmedia = json.socialmedia;
+import Instagram from '@/assets/image/instagram.svg';
+import Facebook from '@/assets/image/facebook.svg';
+import Whatsapp from '@/assets/image/whatsapp.svg';
+import Google from '@/assets/image/google.svg';
+import Maps from '@/assets/image/google-maps.svg';
+const socialmedia = [
+    {
+        "name": "Instagram",
+        "link": "https://www.instagram.com/cendanamotor/",
+        "icon": Instagram
+    },
+    {
+        "name": "Facebook",
+        "link": "https://www.facebook.com/cendanamotor/",
+        "icon": Facebook
+    },
+    {
+        "name": "Whatsapp",
+        "link": "https://wa.me/6281280000000",
+        "icon": Whatsapp
+    },
+    {
+        "name": "Google",
+        "link": "https://wa.me/6281280000000",
+        "icon": Google
+    },
+    {
+        "name": "Maps",
+        "link": "https://goo.gl/maps/5bT3mGaNmFmHAJtFA",
+        "icon": Maps
+    }
+]
 </script>
 
 <template>
     <div id="Kontak">
         <Content>
-            <div>
-                <h1>Kontak</h1>
-                <p>Hubungi kami melalui kontak berikut:</p>
-                <p>Telepon: 08123456789</p>
+            <div class="Box">
+                <h1>Temukan kami!</h1>
             </div>
-            <Contact v-for="item in socialmedia" :name="item.name" src="test" :href="item.link" />
+            <div class="Container">
+                <Contact v-for="item in socialmedia" :name="item.name" :icon="item.icon" :href="item.link" />
+            </div>
         </Content>
     </div>
 </template>
@@ -21,6 +52,33 @@ const socialmedia = json.socialmedia;
     min-height: 100vh;
     width: 100%;
     background-color: rgb(248, 248, 248);
-    color: white;
+    color: rgb(78, 78, 78);
+    padding-top: 30px;
+}
+
+h1 {
+    font-size: 40px;
+    margin-bottom: 2rem;
+    font-weight: 700;
+    text-align: center;
+
+    @media screen and (max-width: 1000px) {
+        font-size: 28px;
+    }
+}
+
+.Container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row !important;
+
+    @media screen and (max-width: 1000px) {
+        flex-direction: unset !important;
+        justify-content: center;
+        gap: 20px;
+        margin-inline: 10px;
+        flex-wrap: wrap;
+    }
 }
 </style>
